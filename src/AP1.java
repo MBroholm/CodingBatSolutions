8084c7aa0897991ab4aa3776cb4f9f8541642114
Mbroholm <mibr0005@stud.kea.dk>
2025-03-28 20:17:41 +0100

    scores100, scoresClump, scoresAverage, wordsCount, wordsFront, wordsWithoutList,

--- src/AP1.java
+++ src/AP1.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AP1 {
     public boolean scoresIncreasing(int[] scores) {
         for(int i = 0; i<scores.length-1; i++){
@@ -9,5 +12,65 @@ public class AP1 {
         return true;
     }
 
+    public boolean scores100(int[] scores) {
+        for(int i = 0; i<scores.length-1; i++){
+            if(scores[i]==100 && scores[i+1]==100) return true;
+        }
+
+        return false;
+    }
+
+    public boolean scoresClump(int[] scores) {
+        for(int i = 0; i<scores.length-2; i++){
+            int diff1 = Math.abs(scores[i]-scores[i+2]);
+            if(diff1 <= 2) return true;
+        }
+        return false;
+    }
+
+    public int scoresAverage(int[] scores) {
+        int split = scores.length/2;
+        int leftSum = average(scores,0,split);
+        int rigthSum = average(scores,split,scores.length);
+
+        return Math.max(leftSum, rigthSum);
+    }
+
+    public int average(int[] scores, int start, int end){
+        int sum = 0;
+        for(int i = start; i < end; i++){
+            sum+=scores[i];
+        }
+        int len = end-start;
+        return sum/len;
+    }
+
+    public int wordsCount(String[] words, int len) {
+        int count = 0;
+        for(String str : words){
+            if(str.length()==len) count++;
+        }
+        return count;
+    }
+
+    public String[] wordsFront(String[] words, int n) {
+        String[] result = new String[n];
+
+        for(int i = 0; i<result.length; i++){
+            result[i]=words[i];
+        }
+
+        return result;
+    }
+
+    public List wordsWithoutList(String[] words, int len) {
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i<words.length; i++){
+            if(words[i].length() != len){
+                list.add(words[i]);
+            }
+        }
+        return list;
+    }
 
 }
