68a9cef829cb5caef2baad9f81fb72abde5ad7d6
Mbroholm <mibr0005@stud.kea.dk>
2025-03-29 15:59:11 +0100

    bunnyEars

--- src/Recursion1.java
+++ src/Recursion1.java
@@ -7,4 +7,12 @@ public class Recursion1 {
         }
     }
 
+    public int bunnyEars(int bunnies) {
+        if(bunnies == 0){
+            return 0;
+        }else{
+            return 2+bunnyEars(bunnies-1);
+        }
+    }
+
 }
