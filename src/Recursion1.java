af8d83bc100242eb3ddaa086954ab0aa8540c40e
Mbroholm <mibr0005@stud.kea.dk>
2025-03-30 11:37:37 +0200

    fibonacci, bunnyEars2, triangle, sumDigits,

--- src/Recursion1.java
+++ src/Recursion1.java
@@ -15,4 +15,30 @@ public class Recursion1 {
         }
     }
 
+    public int fibonacci(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public int bunnyEars2(int bunnies) {
+        if(bunnies==0){
+            return 0;
+        }else if(bunnies%2==0){
+            return 3+bunnyEars2(bunnies-1);
+        }else{
+            return 2+bunnyEars2(bunnies-1);
+        }
+    }
+
+    public int triangle(int rows) {
+        if(rows == 0) return 0;
+        return rows + triangle(rows-1);
+    }
+
+    public int sumDigits(int n) {
+        if(n==0) return 0;
+        return n%10 + sumDigits(n/10);
+    }
+
 }
